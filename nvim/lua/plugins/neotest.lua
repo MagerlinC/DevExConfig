44fb0a9224b376784bfc4fbc0a307ed67120c00f
Mikkel Agerlin <ma@complycloud.com>
2024-08-19 15:38:17 +0200

    feat: tmux window renumbering, playwright test, various minor fixes

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -8,14 +8,14 @@ return {
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-jest")({
-            jestCommand = "npm test --",
-            jestConfigFile = "custom.jest.config.ts",
-            env = { CI = true },
-            cwd = function()
-              return vim.fn.getcwd() .. "/frontend/tests"
-            end,
-          }),
+          -- require("neotest-jest")({
+          --   jestCommand = "npm test --",
+          --   jestConfigFile = "custom.jest.config.ts",
+          --   env = { CI = true },
+          --   cwd = function()
+          --     return vim.fn.getcwd() .. "/frontend/tests"
+          --   end,
+          -- }),
           require("neotest-playwright").adapter({
             options = {
               persist_project_selection = true,
