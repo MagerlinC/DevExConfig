be4a125b3a6a1932f2f06e2fee3743447ae2b072
Mikkel Agerlin <ma@complycloud.com>
2024-10-04 00:47:28 +0200

    feat: harpoon, hop

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,6 +11,26 @@ map("i", "jk", "<ESC>")
 
 map("n", "gb", "<C-o>", { silent = true, desc = "Go back" })
 
+-- Hop
+-- place this in one of your configuration file(s)
+local hop = require "hop"
+local directions = require("hop.hint").HintDirection
+map("", "<leader>hw", function()
+  hop.hint_words()
+end, { desc = "Hop word" })
+map("", "f", function()
+  hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true }
+end, { remap = true })
+map("", "F", function()
+  hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true }
+end, { remap = true })
+map("", "t", function()
+  hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
+end, { remap = true })
+map("", "T", function()
+  hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
+end, { remap = true })
+
 -- Navigate buffers
 map("n", "H", ":bp<CR>", { silent = true, desc = "Prev buffer" })
 map("n", "L", ":bn<CR>", { silent = true, desc = "Next buffer" })
@@ -35,3 +55,15 @@ map("n", "<leader>sg", builtin.live_grep, { desc = "Telescope live grep" })
 
 -- NvChad Terminal
 map("t", "<Esc>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
+map("n", "<leader>th", function()
+  require("nvchad.term").new { pos = "sp" }
+end, { desc = "terminal new horizontal term" })
+
+-- Harpoon
+map("n", "<leader>H", function()
+  require("harpoon.mark").add_file()
+end, { desc = "Harpoon add mark" })
+
+map("n", "<leader>h", function()
+  require("harpoon.ui").toggle_quick_menu()
+end, { desc = "Harpoon toggle quick menu" })
