a933da87aab3595d718445731afde85ec242cac5
Mikkel Agerlin <ma@complycloud.com>
2024-11-26 11:05:26 +0100

    wip: tmux startup script

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
-tmux new-session -d 'work'
-tmux new-window 'serve'
-tmux new-window 'cmds'
-tmux new-window 'nvim'
-tmux new-session -d 'config'
-tmux new-window '.config'
-tmux new-window 'repo'
-tmux attach -d -t work
+# Work session
+tmux new -s work -d
+
+# Serve window
+tmux rename-window -t work serve
+tmux send-keys -t work 'cd ~/git/Cirrus/' C-m
+
+# Cmds window
+tmux new-window -t work
+tmux rename-window -t work cmds 
+tmux send-keys -t work 'cd ~/git/Cirrus/' C-m
+
+# Main editor window
+tmux new-window -t work
+tmux rename-window -t work cirrus 
+tmux send-keys -t work 'cd ~/git/Cirrus/frontend' C-m
+tmux send-keys -t work 'nvim' C-m
+
+
+# Config session
+tmux new -s config -d
+
+# Config window
+tmux rename-window -t config config
+tmux send-keys -t config 'cd ~/.config' C-m
+
+# DevEx Repo window
+tmux new-window -t config 
+tmux rename-window -t config devex
+tmux send-keys -t config 'cd ~/git/dev-ex-config/' C-m
+tmux send-keys -t config 'nvim' C-m
+
+
+tmux select-window -t work:1
+tmux attach -t work
